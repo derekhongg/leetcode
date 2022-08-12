2ac0a5eee0555d483276bcca1d57d9c59f0092d9
derekhongg <hongderek@gmail.com>
2022-08-12 15:49:42 -0700

    new problem added

---
+++ 0812.js
@@ -0,0 +1,34 @@
+/* Decode String
+
+Example: s = "3[a]2[bc]" output = "aaabcbc"
+Input: s = "3[a2[c]]" Output: "accaccacc"
+
+k = how many times we need to repeat it
+[ = Start storing what I want to repeat
+] = Start repeating after this closing bracket
+
+*/
+
+// example : 31[a2[c]]
+
+function decodeString(s) {
+    let multiply = [];
+    let repeatStr = [];
+    let solution = "";
+    let tempMult = "";
+    for(let i of s) {
+        if(!isNaN(i)) {
+            tempMult =  `${tempMult}${i}` //31
+        } else if (i === '[') {
+            multiply.push(tempMult); // [31]
+            tempMult = "";
+            repeatStr.push(solution); // [""]
+            solution = "";
+        } else if (i === ']') {
+            solution = repeatStr.pop() + solution.repeat(multiply.pop())// acc
+        } else {
+            solution += i; // "" + a
+        }
+    }
+    return solution;
+}
