d26d90023f89082d5c0c6a822d6a5ea34f8ff1e3
derekhongg <hongderek@gmail.com>
2022-08-13 18:46:12 -0700

    new problem added

--- 0813.js
+++ 0813.js
@@ -36,3 +36,42 @@ function detectCycle(head) {
     }
     return false;
 }
+
+/* Linked List Cycle II
+
+Two Pointers - Fast and Slow
+Create pointer
+Run while loop while fast & fast.next exist
+move fast and slow pointers
+if fast and slow equal one another break out of the loop
+if fast and slow don't equal one another, we want to break out of the loop
+then we want to run another while loop while pointer and slow don't equal one another and set it to next;
+return slow or pointer at the end
+
+*/
+
+function detectCycle2(head) {
+    if(!head.next || !head) {
+        return null;
+    }
+    let fast = head;
+    let slow = head;
+    let pointer = head;
+
+    while(fast && fast.next) {
+        fast = fast.next.next;
+        slow = slow.next;
+
+        if(fast === slow) {
+            break;
+        }
+    }
+    if(fast !== slow) {
+        return null;
+    }
+    while(pointer !== slow) {
+        pointer = pointer.next;
+        slow = slow.next
+    }
+    return slow;
+}
