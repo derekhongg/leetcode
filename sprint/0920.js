94eef665686edf296790d434cfc1305a65835019
derekhongg <hongderek@gmail.com>
2022-09-20 11:14:51 -0700

    new solution added:

--- sprint/0920.js
+++ sprint/0920.js
@@ -5,16 +5,23 @@ First Solution
 - Create second for loop to iterate through the array again, it'll start at the index ahead of the first for loop
 - Run conditional to test if the i and j add up to the target and return the values in an array
 
-Additional
-- Return multiple solutions
-
 Time Complexity: O(n) ^ 2 - inner for loop
 Space Complexity: O(n) - new data structure created;
+
+Second Solution (More efficient solution)
+- Create empty hashmap and loop through
+- Create complement variable to assist
+
+Time Complexity: O(n)
+Space Complexity: O(n)
+
+Additional
+- Return multiple solutions
 
 */
 
 function twoSum(nums, target) {
-    var solutions = [];
+    let solutions = [];
     for(let i = 0; i < nums.length; i++) {
         for (let j = i + 1; j < nums.length; j++) {
             if(nums[i] + nums[j] === target) {
@@ -29,6 +36,20 @@ function twoSum(nums, target) {
     }
 }
 
+function twoSum2(nums, target) {
+    let solutions = [];
+    let hashmap = {};
+    for(let i = 0; i < nums.length; i++) {
+        let complementValue = target - nums[i];
+        if(hashmap[complementValue] !== undefined) {
+            solutions.push([hashmap[complementValue], i]);
+        } else {
+            hashmap[nums[i]] = i;
+        }
+    }
+    return solutions;
+}
+
 let arr1 =  [1, 2, 3, 4, 5];
 
-console.log(twoSum(arr1, 15));
+console.log(twoSum2(arr1, 7));
