60b2127b03ecf6567ba0c97e3b6123d35e7ad2b0
derekhongg <hongderek@gmail.com>
2022-09-21 16:45:12 -0700

    new problem added

--- sprint/0921.js
+++ sprint/0921.js
@@ -43,3 +43,25 @@ function isPalindrome2(x) {
 }
 
 console.log(1%10);
+
+
+/* Same Tree - Breadth First Search
+
+
+
+
+Time Complexity: O(n)
+Space Complexity: O(n)
+*/
+// if p and q are both null, return true
+// if p is null while q isn't or vise versa return false
+// return isSameTree(left for both) is sameTree right for both
+function isSameTree(p, q) {
+    if (p === null && q === null) {
+        return true;
+    }
+    if((p===null && q !== null) || (p!==null && q===null) || p.val !== q.val) {
+        return false
+    }
+    return isSameTree(p.left, q.left) && isSameTree(p.right,q.right)
+}
