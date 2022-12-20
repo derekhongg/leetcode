7ed403253112062b353cbc55bbb944fe7b10c46e
derekhongg <hongderek@gmail.com>
2022-12-20 12:22:16 -0800

    new problem added

--- algorithm-1/1220.js
+++ algorithm-1/1220.js
@@ -1,16 +1,34 @@
 /* Contains Duplicate */
 
 
-function containsDupe (nums) {
+function containsDupe(nums) {
     let numsHash = {};
 
-    for(let i = 0; i < nums.length; i++) {
+    for (let i = 0; i < nums.length; i++) {
         let val = nums[i];
-        if(numsHash[val]) {
+        if (numsHash[val]) {
             return true;
         } else {
             numsHash[val] = 0;
         }
     }
     return false;
+}
+
+function maxSubArray(nums) {
+    let current = nums[0];
+    let total = nums[0];
+
+    for(let i = 0; i < nums.length; i++) {
+        if(current < 0) {
+            current = nums[i]
+        } else {
+            current += nums[i]
+        }
+
+        if(current > total) {
+            total = current;
+        }
+    }
+    return total;
 }
