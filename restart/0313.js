aa9c1954d8f1a56f08b217b50e9eea6bd19a7966
Derek Hong <Derek.Hong@paccar.com>
2023-03-13 13:30:39 -0700

    new problems

---
+++ restart/0313.js
@@ -0,0 +1,34 @@
+/* Binary Search */
+
+
+var binarySearch = function(nums, target) {
+    let start = 0;
+    let end = nums.length - 1;
+
+    while (start <= end) {
+        let middle = Math.floor((start+end)/2);
+
+        if(nums[middle] === target) {
+            return middle;
+        } else if(nums[middle] < target) {
+            start = middle + 1;
+        } else if(nums[middle] > target) {
+            end = middle - 1;
+        }
+    }
+    return -1;
+}
+
+/* Move Zeroes */
+
+var moveZeroes = function(nums) {
+    let nonZeroIndex = 0;
+
+    for(let i = 0; i < nums.length; i++) {
+        if(nums[i] !== 0) {
+            [nums[i], nums[nonZeroIndex]] = [nums[nonZeroIndex], nums[i]];
+            nonZeroIndex++;
+        }
+    }
+    return nums;
+}
